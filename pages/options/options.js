bb8f25a1d8407ccb84afa10adab30cd56a07ae17
haohao <73471452@qq.com>
2020-07-20 00:48:26 +0800

    fix

---
+++ pages/options/options.js
@@ -0,0 +1,34 @@
+Page({
+  data: {
+    optionCards:[
+        {
+            img:'option-1.png',
+            path:''
+        },
+        {
+            img:'option-2.png',
+            path:''
+        },
+        {
+            img:'option-3.png',
+            path:''
+        },
+        {
+            img:'option-4.png',
+            path:''
+        },
+        {
+            img:'option-5.png',
+            path:''
+        },
+        {
+            img:'option-6.png',
+            path:''
+        }
+    ]
+  },
+  onLoad: function () {
+  },
+})
+
+
