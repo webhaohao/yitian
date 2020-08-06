12df0d20563a9390361e9dff11304eb88b9bd8f5
haohao <73471452@qq.com>
2020-08-06 23:11:42 +0800

    fix

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,19 +5,38 @@ Page({
     attendSuccessImg: "",
     canvasImgUrl: '',
     src: "",
-    close:true
+    close:true,
+    isPhoneX:false
   },
   // 点击照相
   takePictures(){
     takePictures();
   },
   onReady(){
+    wx.getSystemInfo({
+      success:(res)=>{
+        console.log(res);
+        // 根据 model 进行判断
+        if(res.model.search('iPhone X') !=-1 || res.model.search('iPhone 11') !=-1 ) {
+            this.setData({
+              isPhoneX:true
+            })
+        }
+      }
+    })
     setInterval(() => {
       const {close} = this.data;
       this.setData({
           close:!close
       })
-  }, 800);
+    }, 800);
+  },
+   // 分享效果
+   onShareAppMessage () {
+    return {
+      title: `益趣识猫`,
+      path: `pages/home/home`
+    }
   },
   onLoad() {
     
